c7726fc7d297a69fe5a74fbe577db08661439b1f
Junho Kim <ploffer11@naver.com>
2020-01-13 17:29:59 +0900

    2020-01-13

---
+++ precache-manifest.cc98e7e4d9cefe5a395427d6f5f2ca0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e804ce35d717b8852e637e70d0957ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4ad940683f364bcbe4",
+    "url": "/static/css/main.c9dc4004.chunk.css"
+  },
+  {
+    "revision": "b2fb17dbcf328a0e31f6",
+    "url": "/static/js/2.b9ba29c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b9ba29c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff4ad940683f364bcbe4",
+    "url": "/static/js/main.1923f0f0.chunk.js"
+  },
+  {
+    "revision": "5a2c2c9aa53a9f776b05",
+    "url": "/static/js/runtime-main.0b962622.js"
+  }
+]);
