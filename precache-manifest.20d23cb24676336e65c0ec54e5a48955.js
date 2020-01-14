8b8bffb8378ec456641dbe8b819a4e0e3e42f513
Junho Kim <ploffer11@naver.com>
2020-01-14 20:21:16 +0900

    2020-01-14

--- precache-manifest.20d23cb24676336e65c0ec54e5a48955.js
+++ precache-manifest.20d23cb24676336e65c0ec54e5a48955.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b5301a87a9b3101e4a1c67eaac6291b",
+    "revision": "d6611804d35d6b74e06f2bf93d4dcf29",
     "url": "/index.html"
   },
   {
-    "revision": "6ee10dc2e81db7fc6d49",
-    "url": "/static/css/main.fd93eb79.chunk.css"
+    "revision": "a8108b5661bb043234a9",
+    "url": "/static/css/main.f2080dec.chunk.css"
   },
   {
     "revision": "7fee312cedd6f2cb8a3b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.05426a24.chunk.js.LICENSE"
   },
   {
-    "revision": "6ee10dc2e81db7fc6d49",
-    "url": "/static/js/main.84fad5bf.chunk.js"
+    "revision": "a8108b5661bb043234a9",
+    "url": "/static/js/main.d8c6fdbd.chunk.js"
   },
   {
     "revision": "5a2c2c9aa53a9f776b05",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02c7e77eb8253c9973247bec2bc3ae2c.js"
+  "/precache-manifest.20d23cb24676336e65c0ec54e5a48955.js"
 );
 
 self.addEventListener('message', (event) => {
